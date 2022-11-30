80189d59a061d38587ce2761a22f87db228646f8
MariacMejia <mariacmejiat@outlook.com>
2022-11-30 13:02:30 +0100

    made package installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+
+name = "let_it_snow",
+version='0.0.1',
+author='MariaC',
+author_email='mariacmejiat@outlook.com',
+packages=find_packages(),
+descrition='graph snowflakes'
+)
